59a69c7c88424771b93f8cc5f0f5619f0654c2e7
Artem Zatula <artemzatula@gmail.com>
2024-11-06 11:29:16 +0200

    feat: store PORT value into .env

---
+++ src/helpers/env.ts
@@ -0,0 +1,5 @@
+import { config } from 'dotenv';
+
+config();
+ 
+export const getPort = () => process.env.PORT || 3000;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getPort } from './helpers/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(getPort());
 }
 bootstrap();
